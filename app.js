a4044063b0baf884f6801e38374f7d1d1d7249ae
Eduard <eduardstancu@yahoo.com>
2020-07-12 11:41:43 +0300

    mod func forEach 2

--- app.js
+++ app.js
@@ -14,5 +14,14 @@ for(const item of arr){
 
 
 arr.forEach((e)=> {
-	console.log('- '+ e+' -')
+	div.innerHTML+=('<li>'+ e+'</li>')
 })
+
+const result = arr.map((e)=>(e=e+'+p')).filter(e=>e!=='bob+p')
+
+console.log(arr)
+console.log(result)
+
+result.map(e=>div.innerHTML+=e+' ')
+
+arr.forEach((item, index)=>console.log(index + ' - '+ item))
