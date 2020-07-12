88d0b34c4ff853e6eb02e6a49dc22173f556c9f9
Eduard <eduardstancu@yahoo.com>
2020-07-12 11:13:31 +0300

    modif func forEach mod1

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 
 const arr= ['alice', 'bob', 'carla', 'jamal'];
-
+let div = document.getElementById('names')
 for(let i=0; i<arr.length; i++){
+	div.innerHTML += '  ' + arr[i]
 	console.log(arr[i])
 }
+
+for(const item of arr){
+	console.log('----------')
+	console.log(item)
+}
+
+
+
+arr.forEach((e)=> {
+	console.log('- '+ e+' -')
+})
